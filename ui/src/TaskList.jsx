e1c689a7f8a5e6b97dc2c042c36a26073507e054
Mampionona33 <ramamps33@gmail.com>
2021-11-16 12:46:38 +0300

    initialisation ui

---
+++ ui/src/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TaskList = () => {
+  return (
+    <React.Fragment>
+      <h1>Place holder Tasklist</h1>
+    </React.Fragment>
+  );
+};
+
+export default TaskList;
