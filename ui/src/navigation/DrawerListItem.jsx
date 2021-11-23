22d732fb0d84ab696c7bbd9538a969689ceb4132
Mampionona33 <ramamps33@gmail.com>
2021-11-23 14:06:40 +0300

    HMR works after adding drawer and navbar

---
+++ ui/src/navigation/DrawerListItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  ListItem,
+  ListItemButton,
+} from '@mui/material';
+export default function DrawerListItem(props) {
+    const icon = props.icon;
+    const itemText = props.itemText;
+    const drawerOpen = props.drawerOpen;
+    const href = props.href;
+    return (
+      <React.Fragment>
+        <ListItem component='a' href={href}>
+          <ListItemButton onClick={() => drawerOpen(false)}>
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText
+              primary={
+                <Typography variant='subtitle1' style={{ color: '#fff' }}>
+                  {itemText}
+                </Typography>
+              }
+            />
+          </ListItemButton>
+        </ListItem>
+      </React.Fragment>
+    );
+  }
--- ui/src/navigation/Navbar.jsx
+++ ui/src/navigation/Navbar.jsx
@@ -5,25 +5,65 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { useState } from 'react';
+
+// import components
+import DrawerListItem from './DrawerListItem.jsx';
 
 export default function Navbar() {
+  // Gestion d'affichage de Drawer
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+    setIsOpen(open);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
-        <Toolbar variant='dense'>
-          <IconButton
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant='h6' color='inherit' component='div'>
-            Task Tracker
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <React.Fragment>
+      {/* Navbar */}
+      <React.Fragment>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position='static'>
+            <Toolbar variant='dense'>
+              <IconButton
+                edge='start'
+                color='inherit'
+                aria-label='menu'
+                sx={{ mr: 2 }}
+                onClick={toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant='h6' color='inherit' component='div'>
+                Task Tracker
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </React.Fragment>
+      {/* Drawer */}
+      <React.Fragment>
+        <Box>
+          <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+            <List>
+              <DrawerListItem
+                drawerOpen={(state) => setIsOpen(state)}
+                icon={<DashboardIcon />}
+                itemText='Dashboard'
+                href='#/Dashboard'
+              />
+            </List>
+          </Drawer>
+        </Box>
+      </React.Fragment>
+    </React.Fragment>
   );
 }
