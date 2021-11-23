6a90013ed40e5e63e5a6729bd1106e908ad43312
Mampionona33 <ramamps33@gmail.com>
2021-11-23 15:04:24 +0300

    undo all change

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Contents from './Contents.jsx';
 import Navbar from './navigation/NavBar.jsx';
 
-export default function Page(params) {
+export default function Page() {
   return (
     <React.Fragment>
       <Navbar />
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
-  },
+  },  
   optimization: {
     splitChunks: {
       name: 'vendor',
