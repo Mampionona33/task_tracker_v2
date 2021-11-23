554de1f7119edef4e690c12411819943b10c9e5d
Mampionona33 <ramamps33@gmail.com>
2021-11-23 12:22:35 +0300

    HMR work after configuring PageNotfound

--- ui/src/Contents.jsx
+++ ui/src/Contents.jsx
@@ -4,14 +4,16 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // import components
 import TaskList from './TaskList.jsx';
 
-const NotFound = () => <h1>Page Not Found</h1>;
+const NotFound = () => {
+  return <h1>Page Not Found</h1>;
+};
 
 export default function Contents() {
   return (
     <Routes>
       <Route path='/' element={<TaskList />} />
       <Route path='/tasklist' element={<TaskList />} />
-      <Route element={<NotFound />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
