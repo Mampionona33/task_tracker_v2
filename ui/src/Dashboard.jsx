c8646bc5f266cf248b1b88520b99207ecc3cb9fb
Mampionona33 <ramamps33@gmail.com>
2021-11-24 10:13:10 +0300

    finish to fecht data from graphql to Dashboard.jsx

--- ui/src/Dashboard.jsx
+++ ui/src/Dashboard.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import dotenv from 'dotenv/config'
 
 export default function DashBoard() {
+  const [data, setData] = useState({ listFiches: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const query = `query listFiches{
+        listFiches{
+          id typeTrav cat
+        }
+      }`;
+      // call GRAPHQL API
+      const queryResult = await axios.post('http://localhost:3000/graphql', {
+        query: query,
+      });
+      // Update component state
+      const result = queryResult.data.data;
+      setData({ listFiches: result.listFiches });
+      console.log(result.listFiches);
+    };
+    fetchData();
+  }, []);
   return (
     <React.Fragment>
       <h1>Place holder dashboard test</h1>
