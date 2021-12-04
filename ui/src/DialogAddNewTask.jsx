68473b20adde8092d02933ff68c9f647c920115e
Mampionona33 <ramamps33@gmail.com>
2021-12-04 11:40:45 +0300

    fetching data from forms

--- ui/src/DialogAddNewTask.jsx
+++ ui/src/DialogAddNewTask.jsx
@@ -26,7 +26,7 @@ export default function DialogAddNewTask({ open, onClose }) {
   const formRef = useRef();
 
   const handleValueChange = (e) => {
-    setValue(e.target.value);	
+    setValue(e.target.value);
   };
   
   async function handleReset(e){
@@ -38,6 +38,8 @@ export default function DialogAddNewTask({ open, onClose }) {
 	 const form = document.forms.addNew
 		const fiche = {
 			numFiche: form.numFiche.value,
+			cat : form.cat.value,
+			statCom : form.comboBoxStateCom.value,
 		}
 	console.log(fiche);
 	setValue('');
@@ -69,144 +71,159 @@ export default function DialogAddNewTask({ open, onClose }) {
         fullWidth
       >
         <DialogTitle id='alert-dialog-title'>{'Add New Task'}</DialogTitle>
-        <DialogContent>
-		<form id='formId' name='addNew' action='/' method='POST' onSubmit={handleSubmit} ref={formRef}>
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(2,1fr)',
-              gap: 1,
-            }}
-          >
-            <Box>
-              <TextField
-                autoFocus
-                margin='dense'
-                id='numFiche'
-                label='Num fiche'
-                type='text'
-                variant='standard'
-                value={value}
-                onChange={handleValueChange}
-              />
-            </Box>
-            <Box>
-              <TextField
-                autoFocus
-                id='cat'
-                margin='dense'
-                type='text'
-                label='Category'
-                variant='standard'
-              />
-            </Box>
-            <Box sx={{ paddingBottom: 0 }}>
-              <Autocomplete
-                disablePortal
-                id='combo-box-list-stat-ivpn'
-                options={listStatCom}
-                size={'small'}
-                sx={{ marginTop: 1.5 }}
-                PaperComponent={({ children }) => (
-                  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
-                )}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label='Status Commercial'
-                    variant='standard'
-                  />
-                )}
-              />
-            </Box>
-            <Box>
-              <TextField
-                autoFocus
-                id='url'
-                margin='dense'
-                type='text'
-                label='Url'
-                variant='standard'
-              />
-            </Box>
-            <Box sx={{ paddingBottom: 0 }}>
-              <Autocomplete
-                disablePortal
-                id='combo-box-list-type-taches'
-                options={listTaches}
-                size={'small'}
-                sx={{ marginTop: 1.5 }}
-                PaperComponent={({ children }) => (
-                  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
-                )}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label='Type de Travail'
-                    variant='standard'
-                  />
-                )}
-              />
-            </Box>
-            <Box sx={{ paddingBottom: 0 }}>
-              <Autocomplete
-                disablePortal
-                id='combo-box-list-stat-ivpn'
-                options={listStatIvpn}
-                size={'small'}
-                sx={{ marginTop: 1.5 }}
-                PaperComponent={({ children }) => (
-                  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
-                )}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label='Status IVPN'
-                    variant='standard'
-                  />
-                )}
-              />
-            </Box>
-            <Box>
-              <TextField
-                autoFocus
-                id='nbBefore'
-                margin='dense'
-                type='text'
-                label='Nb BEFORE'
-                variant='standard'
-              />
-            </Box>
-            <Box>
-              <TextField
-                autoFocus
-                id='nbAfter'
-                margin='dense'
-                type='text'
-                label='Nb AFTER'
-                variant='standard'
-              />
-            </Box>
-          </Box>
-          <Box sx={{ marginTop: '1rem' }}>
-            <Typography fontFamily='sans-serif' color='GrayText'>
-              COMMENT
-            </Typography>
-            <TextareaAutosize
-              id='comment'
-              style={{ width: '100%', minHeight: '4rem' }}
-            />
-          </Box>
-		</form>
-        </DialogContent>
-        <DialogActions>
+        
+		<DialogContent>
+
+			<form id='formId' name='addNew' action='/' method='POST' onSubmit={handleSubmit} ref={formRef}>
+			  <Box
+				sx={{
+				  display: 'grid',
+				  gridTemplateColumns: 'repeat(2,1fr)',
+				  gap: 1,
+				}}
+			  >
+				<Box>
+				  <TextField
+					autoFocus
+					margin='dense'
+					id='numFiche'
+					label='Num fiche'
+					type='text'
+					variant='standard'
+					value={value}
+					onChange={handleValueChange}
+				  />
+				</Box>
+				
+				<Box>
+				  <TextField
+					autoFocus
+					id='cat'
+					margin='dense'
+					type='text'
+					label='Category'
+					variant='standard'
+				  />
+				</Box>
+				
+				<Box sx={{ paddingBottom: 0 }}>
+				  <Autocomplete
+					disablePortal
+					options={listStatCom}
+					size={'small'}
+					id='comboBoxStateCom'
+					sx={{ marginTop: 1.5 }}
+					PaperComponent={({ children }) => (
+					  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
+					)}
+					renderInput={(params) => (
+					  <TextField
+						{...params}
+						label='Status Commercial'
+						variant='standard'
+					  />
+					)}
+				  />
+				</Box>
+				
+				<Box>
+				  <TextField
+					autoFocus
+					id='url'
+					margin='dense'
+					type='text'
+					label='Url'
+					variant='standard'
+				  />
+				</Box>
+				
+				<Box sx={{ paddingBottom: 0 }}>
+				  <Autocomplete
+					disablePortal
+					id='combo-box-type-taches'
+					options={listTaches}
+					size={'small'}
+					sx={{ marginTop: 1.5 }}
+					PaperComponent={({ children }) => (
+					  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
+					)}
+					renderInput={(params) => (
+					  <TextField
+						{...params}
+						label='Type de Travail'
+						variant='standard'
+					  />
+					)}
+				  />
+				</Box>
+				
+				<Box sx={{ paddingBottom: 0 }}>
+				  <Autocomplete
+					disablePortal
+					id='combo-box-list-stat-ivpn'
+					options={listStatIvpn}
+					size={'small'}
+					sx={{ marginTop: 1.5 }}
+					PaperComponent={({ children }) => (
+					  <Paper sx={{ typography: 'body2' }}>{children}</Paper>
+					)}
+					renderInput={(params) => (
+					  <TextField
+						{...params}
+						label='Status IVPN'
+						variant='standard'
+					  />
+					)}
+				  />
+				</Box>
+				
+				<Box>
+				  <TextField
+					autoFocus
+					id='nbBefore'
+					margin='dense'
+					type='text'
+					label='Nb BEFORE'
+					variant='standard'
+				  />
+				</Box>
+				
+				<Box>
+				  <TextField
+					autoFocus
+					id='nbAfter'
+					margin='dense'
+					type='text'
+					label='Nb AFTER'
+					variant='standard'
+				  />
+				</Box>
+			  </Box>
+			  
+			  <Box sx={{ marginTop: '1rem' }}>
+				<Typography fontFamily='sans-serif' color='GrayText'>
+				  COMMENT
+				</Typography>
+				<TextareaAutosize
+				  id='comment'
+				  style={{ width: '100%', minHeight: '4rem' }}
+				/>
+			  
+			  </Box>
+			
+			</form>
+        
+		</DialogContent>
+        
+		<DialogActions>
           <Button onClick={onClose} type='Submit' autoFocus form='formId'>
             Save
           </Button>
           <Button onClick={onClose}>Cancel</Button>
 		  <Button onClick={handleReset} type='reset'>Reset</Button>
         </DialogActions>
-      </Dialog>
+      
+	  </Dialog>
     </div>
   );
 }
