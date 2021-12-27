16ee66769ff16914b945d1170803cd81fdbbf1a0
Mampionona33 <ramamps33@gmail.com>
2021-12-27 12:51:37 -0400

    Page.jsx: setting background height:100vh

--- ui/src/Components/Dashboard.jsx
+++ ui/src/Components/Dashboard.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import graphQLFetch from './graphQLFetch.jsx';
-import { Grid } from '@mui/material';
+import { Grid } from "@mui/material";
 
 // impot components
-import InProgress from './InProgress.jsx';
-import SubmitTask from './SubmitTask.jsx';
-import Processing from './Processing.jsx';
+import InProgress from "./InProgress.jsx";
+import SubmitTask from "./SubmitTask.jsx";
+import Processing from "./Processing.jsx";
 
 export default function DashBoard() {
   return (
@@ -14,8 +14,8 @@ export default function DashBoard() {
         container
         sx={{ padding: 3 }}
         spacing={2}
-        justifyContent='space-evenly'
-        height='95vh'
+        justifyContent="space-evenly"
+        // height='95vh'
       >
         <SubmitTask />
         <InProgress />
--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from "react";
+import { Box } from "@mui/material";
 
-import Contents from './Contents.jsx';
-import Navbar from './Components/NavBar.jsx';
+import Contents from "./Contents.jsx";
+import Navbar from "./Components/NavBar.jsx";
 
 export default function Page() {
   return (
     <React.Fragment>
       <Box
+        height="100vh"
         sx={{
           background:
-            'linear-gradient(55deg, rgba(112,128,144,1) 0%, rgba(192,192,192,1) 36%, rgba(192,192,192,1) 62%, rgba(112,128,144,1) 100%)',
+            "linear-gradient(55deg, rgba(112,128,144,1) 0%, rgba(192,192,192,1) 36%, rgba(192,192,192,1) 62%, rgba(112,128,144,1) 100%)",
         }}
       >
         <Navbar />
