a235772a8792f1e9c6ad1f8ce3a71dbfd862a530
Mampionona33 <ramamps33@gmail.com>
2022-01-24 09:58:05 +0300

    timer without tick work

--- ui/src/Components/PausePlayButton.jsx
+++ ui/src/Components/PausePlayButton.jsx
@@ -1,197 +1,167 @@
-import React, {
-    useEffect,
-    useState,
-    useRef
-}
-from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import { useMutation } from '@apollo/client';
+import { UPDATE_FICHE } from '../GraphQL/Mutation';
+import { LOAD_DATA } from '../GraphQL/Queries';
 import {
-    useMutation
-}
-from '@apollo/client';
-import {
-    UPDATE_FICHE
-}
-from '../GraphQL/Mutation';
-import {
-    LOAD_DATA
-}
-from '../GraphQL/Queries';
-import {
-    loadProcessingPause,
-    loadProcessingPlay,
-    setProcessToPause,
-    setProcessToPlay,
-    getLastupdate,
-    renderDate,
-    updateElastedTime,
-    loadElapstedTime,
-    modifyLastUpdate,
-    intToTimer,
-}
-from './dataHandler';
-import {
-    getUtcDateNow
-}
-from '../Features/getUtcDateNow';
-import {
-    IconButton,
-    Typography,
-    Box
-}
-from '@mui/material';
+  loadProcessingPause,
+  loadProcessingPlay,
+  setProcessToPause,
+  setProcessToPlay,
+  getLastupdate,
+  renderDate,
+  updateElastedTime,
+  loadElapstedTime,
+  modifyLastUpdate,
+  intToTimer,
+} from './dataHandler';
+import { getUtcDateNow } from '../Features/getUtcDateNow';
+import { IconButton, Typography, Box } from '@mui/material';
 
 const PausePlayButton = () => {
-    const[currentProcess, setCurrentProcess] = useState('');
-    const[prevProcessId, setPrevProcessId] = useState(0);
-    const[prevFicheLastUpdate, setPrevFicheLastUpdate] = useState([]);
-    const[prevFicheElapstedTime, setPrevFicheElapstedTime] = useState(0);
-    const[localTimer, setLocalTimer] = useState(0);
-    const[uiTimer, setUiTimer] = useState(0);
-    const[timeInFromLocalStorage, setTimeInFromLocalStorage] = useState(0);
+  const [currentProcess, setCurrentProcess] = useState('');
+  const [prevProcessId, setPrevProcessId] = useState(0);
+  const [prevFicheLastUpdate, setPrevFicheLastUpdate] = useState([]);
+  const [prevFicheElapstedTime, setPrevFicheElapstedTime] = useState(0);
+  const [localTimer, setLocalTimer] = useState(0);
+  const [uiTimer, setUiTimer] = useState(0);
+  const [timeInFromLocalStorage, setTimeInFromLocalStorage] = useState(0);
 
-    const[tickInc, setTickInc] = useState(0);
+  const [tickInc, setTickInc] = useState(0);
 
-    let count = useRef(null);
-    let timerCount = useRef(null);
+  let count = useRef(null);
+  let timerCount = useRef(null);
 
-    // load processing task status
-    const pauseData = loadProcessingPause();
-    const playData = loadProcessingPlay();
-    const lastUpdate = getLastupdate();
-    const dateNow = getUtcDateNow();
+  // load processing task status
+  const pauseData = loadProcessingPause();
+  const playData = loadProcessingPlay();
+  const lastUpdate = getLastupdate();
+  const dateNow = getUtcDateNow();
 
-    // execute mutation fichesUpdate with useMutation
-    const[fichesUpdate, {
-            error: erroUpDate
-        }
-    ] = useMutation(UPDATE_FICHE, {
-        refetchQueries: [LOAD_DATA],
+  // execute mutation fichesUpdate with useMutation
+  const [fichesUpdate, { error: erroUpDate }] = useMutation(UPDATE_FICHE, {
+    refetchQueries: [LOAD_DATA],
+  });
+
+  useEffect(() => {
+    if (playData.length > 0) {
+      setPrevProcessId((prevId) => playData[0].id);
+      setCurrentProcess((prev) => playData[0].processing);
+      setPrevFicheLastUpdate((prev) => playData[0].lastUpdate);
+      setPrevFicheElapstedTime((prev) => playData[0].elapstedTime);
+      setUiTimer(
+        (prev) =>
+          (Date.parse(new Date()) - Date.parse(prevFicheLastUpdate)) / 1000 +
+          prevFicheElapstedTime
+      );
+    }
+
+    if (pauseData.length > 0) {
+      setCurrentProcess((prev) => pauseData[0].processing);
+      setPrevProcessId((prevId) => pauseData[0].id);
+      setPrevFicheLastUpdate((prev) => pauseData[0].lastUpdate);
+      setPrevFicheElapstedTime((prev) => pauseData[0].elapstedTime);
+    }
+
+    if (currentProcess === 'isPlay') {
+      //   timerCount.current = setInterval(() => tick(), 1000);
+    }
+
+    if (currentProcess === 'isPause') {
+      //   clearInterval(timerCount.current);
+    }
+
+    if (prevFicheLastUpdate.length > 0) {
+      //   const dateNowGmt = Date.parse(new Date().toUTCString());
+      //   const prevLastUpdateDate = Date.parse(prevFicheLastUpdate);
+      //   let diffDate = (dateNowGmt - prevLastUpdateDate) / 1000;
+      //   localStorage.setItem('localTimerInt', diffDate);
+    }
+
+    window.addEventListener('storage', (e) => {
+      // console.log(parseInt(localStorage.getItem('localTimerInt')));
+      //   setUiTimer((prev) => parseInt(localStorage.getItem('localTimerInt')));
     });
+  }, [pauseData, playData, currentProcess, prevFicheLastUpdate]);
 
-    useEffect(() => {
-        if (playData.length > 0) {
-            setCurrentProcess((prev) => playData[0].processing);
-            setPrevProcessId((prevId) => playData[0].id);
-            setPrevFicheLastUpdate((prev) => playData[0].lastUpdate);
-            setPrevFicheElapstedTime((prev) => playData[0].elapstedTime);
+  //   console.log('prevElapstime', prevFicheElapstedTime);
+  //   console.log('playData', prevFicheLastUpdate);
 
-        }
+  // const tick = () => {
+  // setTickInc((prev) => prev + 1);
+  // };
 
-        if (pauseData.length > 0) {
-            setCurrentProcess((prev) => pauseData[0].processing);
-            setPrevProcessId((prevId) => pauseData[0].id);
-            setPrevFicheLastUpdate((prev) => pauseData[0].lastUpdate);
-            setPrevFicheElapstedTime((prev) => pauseData[0].elapstedTime);
-        }
+  const tick = () => {
+    let prevLocalTime = parseInt(localStorage.getItem('localTimerInt'));
+    localStorage.setItem('localTimerInt', ++prevLocalTime);
+    window.dispatchEvent(new Event('storage'));
+  };
 
-        if (currentProcess === 'isPlay') {
-            timerCount.current = setInterval(() => tick(), 1000);
-        }
+  const handleClickPlay = async (e) => {
+    await setProcessToPlay(prevProcessId, fichesUpdate, erroUpDate).then(
+      modifyLastUpdate(prevProcessId, fichesUpdate, erroUpDate)
+    );
+  };
 
-        if (currentProcess === 'isPause') {
-            clearInterval(timerCount.current);
-        }
+  const handleClickPause = async (e) => {
+    const elapstedTime =
+      (Date.parse(new Date()) - Date.parse(prevFicheLastUpdate)) / 1000 +
+      prevFicheElapstedTime;
 
-        if (prevFicheLastUpdate.length > 0) {
+    console.log('elapstedTime', elapstedTime);
+    console.log('prevProcessId', prevProcessId);
 
-            const dateNowGmt = Date.parse(new Date().toUTCString());
-            const prevLastUpdateDate = Date.parse(prevFicheLastUpdate);
+    // await setProcessToPause(prevProcessId, fichesUpdate, erroUpDate).then(
+    //   modifyLastUpdate(prevProcessId, fichesUpdate, erroUpDate).then(
+    await updateElastedTime(
+      prevProcessId,
+      Math.floor(elapstedTime),
+      fichesUpdate,
+      erroUpDate
+    )
+      .then(setProcessToPause(prevProcessId, fichesUpdate, erroUpDate))
+      .then(modifyLastUpdate(prevProcessId, fichesUpdate, erroUpDate));
 
-            let diffDate = (dateNowGmt - prevLastUpdateDate) / 1000;
+    // clearInterval(timerCount.current);
+  };
 
-            console.log(dateNowGmt);
-            console.log(new Date().toUTCString());
-            console.log('Date.parse(prevFicheLastUpdate)', prevFicheLastUpdate);
-            console.log('diffDate', diffDate);
-            // console.log(dateNowGmt.getUTCDate());
-            // console.log(dateNowGmt.getUTCFullYear());
-            // console.log('dateNow', dateNowGmt);
-            // console.log('Date.parse(new Date().toUTCString()', Date.parse(new Date().toUTCString()));
-            // console.log('diffDate', diffDate);
-
-
-            localStorage.setItem('localTimerInt', diffDate);
-        }
-
-        window.addEventListener('storage', (e) => {
-            // console.log(parseInt(localStorage.getItem('localTimerInt')));
-            setUiTimer((prev) => parseInt(localStorage.getItem('localTimerInt')));
-        });
-
-    }, [pauseData, playData, currentProcess, prevFicheLastUpdate]);
-
-    // const tick = () => {
-    // setTickInc((prev) => prev + 1);
-    // };
-
-    const tick = () => {
-        let prevLocalTime = parseInt(localStorage.getItem('localTimerInt'));
-        localStorage.setItem('localTimerInt', ++prevLocalTime)
-        window.dispatchEvent(new Event('storage'));
-    };
-
-    const stopTick = () => {}
-
-    const handleClickPlay = async(e) => {
-        await setProcessToPlay(prevProcessId, fichesUpdate, erroUpDate);
-    };
-
-    const handleClickPause = async(e) => {
-
-        const elapstedTime = (Date.parse(new Date()) - Date.parse(prevFicheLastUpdate)) + prevFicheElapstedTime;
-
-        console.log('elapstedTime', elapstedTime);
-        console.log('prevProcessId', prevProcessId);
-
-        await setProcessToPause(prevProcessId, fichesUpdate, erroUpDate).then(
-            modifyLastUpdate(prevProcessId, fichesUpdate, erroUpDate).then(
-                updateElastedTime(prevProcessId, elapstedTime, fichesUpdate, erroUpDate)));
-        // clearInterval(timerCount.current);
-    };
-
-    const ButtonPlay = () => {
-        return (
-             < IconButton
-            color = 'primary'
-                component = 'span'
-                label = 'Play button'
-                onClick = {
-                handleClickPlay
-            }
-             >
-             < PlayCircleIcon /  >
-             <  / IconButton > );
-    };
-
-    const ButtonPause = () => {
-        return (
-             < IconButton
-            color = 'primary'
-                component = 'span'
-                label = 'Pause button'
-                onClick = {
-                handleClickPause
-            }
-             >
-             < PauseCircleIcon /  >
-             <  / IconButton > );
-    };
-
+  const ButtonPlay = () => {
     return (
-         < Box sx = { {
-                margin: 5
-            }
-        }
-         >
-         < Typography > elapsted time: {
-        intToTimer(uiTimer)
-    }
-         <  / Typography > {
-        currentProcess === 'isPlay' ?  < ButtonPause /  >  :  < ButtonPlay /  >
-    }
-         <  / Box > );
+      <IconButton
+        color='primary'
+        component='span'
+        label='Play button'
+        onClick={handleClickPlay}
+      >
+        <PlayCircleIcon />
+      </IconButton>
+    );
+  };
+
+  const ButtonPause = () => {
+    return (
+      <IconButton
+        color='primary'
+        component='span'
+        label='Pause button'
+        onClick={handleClickPause}
+      >
+        <PauseCircleIcon />
+      </IconButton>
+    );
+  };
+
+  return (
+    <Box
+      sx={{
+        margin: 5,
+      }}
+    >
+      <Typography> elapsted time: {intToTimer(uiTimer)}</Typography>{' '}
+      {currentProcess === 'isPlay' ? <ButtonPause /> : <ButtonPlay />}
+    </Box>
+  );
 };
 
 export default PausePlayButton;
--- ui/src/Components/dataHandler.js
+++ ui/src/Components/dataHandler.js
@@ -1,342 +1,325 @@
-import {
-    useQuery,
-    useMutation,
-    gql
-}
-from '@apollo/client';
-import {
-    LOAD_DATA,
-    FILTRED_FICHE
-}
-from '../GraphQL/Queries';
-import {
-    UPDATE_FICHE
-}
-from '../GraphQL/Mutation';
-import React, {
-    useEffect,
-    useState,
-    useRef
-}
-from 'react';
-import {
-    formatNbr
-}
-from '../Features/formatNbr';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import { LOAD_DATA, FILTRED_FICHE } from '../GraphQL/Queries';
+import { UPDATE_FICHE } from '../GraphQL/Mutation';
+import React, { useEffect, useState, useRef } from 'react';
+import { formatNbr } from '../Features/formatNbr';
 
 // FETHING DATA-----------------------------
 
 // fetch global data
 function loadAllData() {
-    const[output, setOutpout] = useState([]);
-    const {
-        data: allData,
-        loading: allDataLoading,
-        error: errorLoadData,
-    } = useQuery(LOAD_DATA);
+  const [output, setOutpout] = useState([]);
+  const {
+    data: allData,
+    loading: allDataLoading,
+    error: errorLoadData,
+  } = useQuery(LOAD_DATA);
 
-    useEffect(() => {
-        if (allData) {
-            setOutpout(allData);
-        }
-    }, [allData]);
-    return output;
+  useEffect(() => {
+    if (allData) {
+      setOutpout(allData);
+    }
+  }, [allData]);
+  return output;
 }
 
 // Fetch the current play Task
 function loadProcessingPlay() {
-    const[outputPlay, setOuputPlay] = useState([]);
+  const [outputPlay, setOuputPlay] = useState([]);
 
-    const {
-        error: errorPause,
-        loading: loadingPause,
-        data: playdata,
-    } = useQuery(FILTRED_FICHE, {
-        variables: {
-            input: {
-                processing: 'isPlay',
-            },
-        },
-    });
-    useEffect(() => {
-        if (playdata) {
-            setOuputPlay(playdata.searchFiches);
-            // console.log(playdata.searchFiches);
-        }
-    }, [playdata]);
-    return outputPlay;
+  const {
+    error: errorPause,
+    loading: loadingPause,
+    data: playdata,
+  } = useQuery(FILTRED_FICHE, {
+    variables: {
+      input: {
+        processing: 'isPlay',
+      },
+    },
+  });
+  useEffect(() => {
+    if (playdata) {
+      setOuputPlay(playdata.searchFiches);
+      // console.log(playdata.searchFiches);
+    }
+  }, [playdata]);
+  return outputPlay;
 }
 
 // Fetch the current played Task
 function loadProcessingPause(params) {
-    const[outputPause, setOuputPause] = useState([]);
+  const [outputPause, setOuputPause] = useState([]);
 
-    const {
-        error: errorPause,
-        loading: loadingPause,
-        data: dataPause,
-    } = useQuery(FILTRED_FICHE, {
-        variables: {
-            input: {
-                processing: 'isPause',
-            },
-        },
-    });
-    useEffect(() => {
-        if (dataPause) {
-            setOuputPause(dataPause.searchFiches);
-            // console.log('dataPause',dataPause);
-        }
-    }, [dataPause]);
-    return outputPause;
+  const {
+    error: errorPause,
+    loading: loadingPause,
+    data: dataPause,
+  } = useQuery(FILTRED_FICHE, {
+    variables: {
+      input: {
+        processing: 'isPause',
+      },
+    },
+  });
+  useEffect(() => {
+    if (dataPause) {
+      setOuputPause(dataPause.searchFiches);
+      // console.log('dataPause',dataPause);
+    }
+  }, [dataPause]);
+  return outputPause;
 }
 
 // Fetching unsubmited task
 function loadUnsubmitedTask() {
-    const[outpoutUnsubmited, setOutpoutUnsubmited] = useState([]);
-    const {
-        error: errorUnsubmited,
-        loading: loadingUnsumbited,
-        data: dataUnsubmited,
-    } = useQuery(FILTRED_FICHE, {
-        variables: {
-            input: {
-                submiteState: 'isUnsubmited',
-            },
-        },
-    });
-    useEffect(() => {
-        if (dataUnsubmited) {
-            setOutpoutUnsubmited(dataUnsubmited.searchFiches);
-        }
-    });
-    return outpoutUnsubmited;
+  const [outpoutUnsubmited, setOutpoutUnsubmited] = useState([]);
+  const {
+    error: errorUnsubmited,
+    loading: loadingUnsumbited,
+    data: dataUnsubmited,
+  } = useQuery(FILTRED_FICHE, {
+    variables: {
+      input: {
+        submiteState: 'isUnsubmited',
+      },
+    },
+  });
+  useEffect(() => {
+    if (dataUnsubmited) {
+      setOutpoutUnsubmited(dataUnsubmited.searchFiches);
+    }
+  });
+  return outpoutUnsubmited;
 }
 
 // Fetching submited task
 function loadSubmitedTask() {
-    const[outpoutSubmited, setOutpoutSubmited] = useState([]);
-    const {
-        error: errorUnsubmited,
-        loading: loadingUnsumbited,
-        data: dataSubmited,
-    } = useQuery(FILTRED_FICHE, {
-        variables: {
-            input: {
-                submiteState: 'isSubmited',
-            },
-        },
-    });
-    useEffect(() => {
-        if (dataSubmited) {
-            setOutpoutSubmited(dataSubmited.searchFiches);
-        }
-    });
-    return outpoutSubmited;
+  const [outpoutSubmited, setOutpoutSubmited] = useState([]);
+  const {
+    error: errorUnsubmited,
+    loading: loadingUnsumbited,
+    data: dataSubmited,
+  } = useQuery(FILTRED_FICHE, {
+    variables: {
+      input: {
+        submiteState: 'isSubmited',
+      },
+    },
+  });
+  useEffect(() => {
+    if (dataSubmited) {
+      setOutpoutSubmited(dataSubmited.searchFiches);
+    }
+  });
+  return outpoutSubmited;
 }
 
 // Fetching elapstedTime
 const loadElapstedTime = () => {
-    const[outpoutElapstedTime, setOutpoutElapstedTime] = useState([]);
-    const playData = loadProcessingPlay();
-    const pauseData = loadProcessingPause();
+  const [outpoutElapstedTime, setOutpoutElapstedTime] = useState([]);
+  const playData = loadProcessingPlay();
+  const pauseData = loadProcessingPause();
 
-    useEffect(() => {
-        if (playData.length > 0) {
-            setOutpoutElapstedTime(playData[0].elapstedTime);
-        }
-        if (pauseData.length > 0) {
-            setOutpoutElapstedTime(pauseData[0].elapstedTime);
-        }
-    }, [playData, pauseData]);
+  useEffect(() => {
+    if (playData.length > 0) {
+      setOutpoutElapstedTime(playData[0].elapstedTime);
+    }
+    if (pauseData.length > 0) {
+      setOutpoutElapstedTime(pauseData[0].elapstedTime);
+    }
+  }, [playData, pauseData]);
 
-    return outpoutElapstedTime;
+  return outpoutElapstedTime;
 };
 
 // MUTATE DATA-----------------------------
 // set prevProcessPlay to off
-const setPrevProcessIsOff = async(
-        prevProcessPlayId,
-        fichesUpdate,
-        erroUpDate) => {
-    fichesUpdate({
-        variables: {
-            filter: {
-                id: prevProcessPlayId,
-            },
-            update: {
-                processing: 'isOff',
-            },
-        },
-    });
-    if (erroUpDate) {
-        console.log(erroUpDate);
-    }
-    return prevProcessPlayId;
+const setPrevProcessIsOff = async (
+  prevProcessPlayId,
+  fichesUpdate,
+  erroUpDate
+) => {
+  fichesUpdate({
+    variables: {
+      filter: {
+        id: prevProcessPlayId,
+      },
+      update: {
+        processing: 'isOff',
+      },
+    },
+  });
+  if (erroUpDate) {
+    console.log(erroUpDate);
+  }
+  return prevProcessPlayId;
 };
 
 // set prevProcessPlay to pause
-const setProcessToPause = async(prevProcessId, fichesUpdate, errorUpDate) => {
-    fichesUpdate({
-        variables: {
-            filter: {
-                id: prevProcessId,
-            },
-            update: {
-                processing: 'isPause',
-            },
-        },
-    });
-    if (errorUpDate) {
-        console.log(errorUpDate);
-    }
-    return prevProcessId;
+const setProcessToPause = async (prevProcessId, fichesUpdate, errorUpDate) => {
+  fichesUpdate({
+    variables: {
+      filter: {
+        id: prevProcessId,
+      },
+      update: {
+        processing: 'isPause',
+      },
+    },
+  });
+  if (errorUpDate) {
+    console.log(errorUpDate);
+  }
+  return prevProcessId;
 };
 // set prevProcessPause to Play
-const setProcessToPlay = async(prevProcessId, fichesUpdate, errorUpDate) => {
-    fichesUpdate({
-        variables: {
-            filter: {
-                id: prevProcessId,
-            },
-            update: {
-                processing: 'isPlay',
-            },
-        },
-    });
-    if (errorUpDate) {
-        console.log(errorUpDate);
-    }
-    return prevProcessId;
+const setProcessToPlay = async (prevProcessId, fichesUpdate, errorUpDate) => {
+  fichesUpdate({
+    variables: {
+      filter: {
+        id: prevProcessId,
+      },
+      update: {
+        processing: 'isPlay',
+      },
+    },
+  });
+  if (errorUpDate) {
+    console.log(errorUpDate);
+  }
+  return prevProcessId;
 };
 
 const getLastupdate = () => {
-    const playProcess = loadProcessingPlay();
-    const pauseProcess = loadProcessingPause();
-    const[lastUpdate, setLastUpdate] = useState([]);
+  const playProcess = loadProcessingPlay();
+  const pauseProcess = loadProcessingPause();
+  const [lastUpdate, setLastUpdate] = useState([]);
 
-    useEffect(() => {
-        if (playProcess.length > 0) {
-            setLastUpdate(playProcess[0].lastUpdate);
-        }
-        if (pauseProcess.length > 0) {
-            setLastUpdate(pauseProcess[0].lastUpdate);
-        }
-    }, [playProcess, pauseProcess, lastUpdate]);
+  useEffect(() => {
+    if (playProcess.length > 0) {
+      setLastUpdate(playProcess[0].lastUpdate);
+    }
+    if (pauseProcess.length > 0) {
+      setLastUpdate(pauseProcess[0].lastUpdate);
+    }
+  }, [playProcess, pauseProcess, lastUpdate]);
 
-    return Date.parse(lastUpdate);
+  return Date.parse(lastUpdate);
 };
 
 // use do render date frome string to clock
 const renderDate = (value) => {
-    console.log(value);
-    const[outPut, setOutput] = useState(``);
-    const[day, setDay] = useState(0);
-    const[hours, setHours] = useState(0);
-    const[min, setMin] = useState(0);
-    const[sec, setSec] = useState(0);
-    const[milSec, setMilSec] = useState(0);
+  console.log(value);
+  const [outPut, setOutput] = useState(``);
+  const [day, setDay] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [milSec, setMilSec] = useState(0);
 
-    useEffect(() => {
-        if (value) {
-            setDay((prev) =>
-                Math.floor((value % 86400) / 36000)
-                .toString()
-                .padStart(2, '0'));
-            setHours((prev) =>
-                Math.floor((value % 86400) / 3600)
-                .toString()
-                .padStart(2, '0'));
-            setMin((prev) =>
-                Math.floor((value % 3600) / 60)
-                .toString()
-                .padStart(2, '0'));
-            setSec((prev) =>
-                Math.floor(value % 60)
-                .toString()
-                .padStart(2, '0'));
-        }
-    }, [value]);
-    return `${day}:${hours}:${min}:${sec}`;
+  useEffect(() => {
+    if (value) {
+      setDay((prev) =>
+        Math.floor((value % 86400) / 36000)
+          .toString()
+          .padStart(2, '0')
+      );
+      setHours((prev) =>
+        Math.floor((value % 86400) / 3600)
+          .toString()
+          .padStart(2, '0')
+      );
+      setMin((prev) =>
+        Math.floor((value % 3600) / 60)
+          .toString()
+          .padStart(2, '0')
+      );
+      setSec((prev) =>
+        Math.floor(value % 60)
+          .toString()
+          .padStart(2, '0')
+      );
+    }
+  }, [value]);
+  return `${day}:${hours}:${min}:${sec}`;
 };
 
 const intToTimer = (value) => {
-    let day = Math.floor((value % 86400) / 36000)
-        .toString()
-        .padStart(2, '0');
-    let hours = Math.floor((value % 86400) / 3600)
-        .toString()
-        .padStart(2, '0');
-    let min = Math.floor((value % 3600) / 60)
-        .toString()
-        .padStart(2, '0');
-    let sec = Math.floor(value % 60)
-        .toString()
-        .padStart(2, '0');
+  let day = Math.floor((value % 86400) / 36000)
+    .toString()
+    .padStart(2, '0');
+  let hours = Math.floor((value % 86400) / 3600)
+    .toString()
+    .padStart(2, '0');
+  let min = Math.floor((value % 3600) / 60)
+    .toString()
+    .padStart(2, '0');
+  let sec = Math.floor(value % 60)
+    .toString()
+    .padStart(2, '0');
 
-    return `${day}:${hours}:${min}:${sec}`;
-}
+  return `${day}:${hours}:${min}:${sec}`;
+};
 
-const updateElastedTime = async(
-        currentProcessId,
-        elapstedTime,
-        fichesUpdate,
-        errorUpDate) => {
-    fichesUpdate({
-        variables: {
-            filter: {
-                id: currentProcessId,
-            },
-            update: {
-                elapstedTime: elapstedTime,
-            },
-        },
-    });
-    if (errorUpDate) {
-        console.log(errorUpDate);
-    }
-    return currentProcessId;
+const updateElastedTime = async (
+  currentProcessId,
+  elapstedTime,
+  fichesUpdate,
+  errorUpDate
+) => {
+  fichesUpdate({
+    variables: {
+      filter: {
+        id: currentProcessId,
+      },
+      update: {
+        elapstedTime: elapstedTime,
+      },
+    },
+  });
+  if (errorUpDate) {
+    console.log(errorUpDate);
+  }
+  return currentProcessId;
 };
 
 // set the current time the last update
-const modifyLastUpdate = async(
-        currentFicheId,
-        fichesUpdate,
-        errorUpDate) => {
-    const dateNow = new Date();
+const modifyLastUpdate = async (currentFicheId, fichesUpdate, errorUpDate) => {
+  const dateNow = new Date();
+  //   const utcDateNow = new Date(
+  //     dateNow.getTime() - dateNow.getTimezoneOffset() * 60000
+  //   ).toISOString();
 
-    const utcDateNow = new Date(
-            dateNow.getTime() - dateNow.getTimezoneOffset() * 60000).toISOString();
-
-    fichesUpdate({
-        variables: {
-            filter: {
-                id: currentFicheId,
-            },
-            update: {
-                lastUpdate: utcDateNow,
-            },
-        },
-    });
-    if (errorUpDate) {
-        console.log(errorUpDate);
-    }
-    return date;
+  fichesUpdate({
+    variables: {
+      filter: {
+        id: currentFicheId,
+      },
+      update: {
+        lastUpdate: dateNow,
+      },
+    },
+  });
+  if (errorUpDate) {
+    console.log(errorUpDate);
+  }
+  return dateNow;
 };
 
 export {
-    loadProcessingPause,
-    loadAllData,
-    loadProcessingPlay,
-    loadUnsubmitedTask,
-    loadSubmitedTask,
-    setPrevProcessIsOff,
-    setProcessToPause,
-    setProcessToPlay,
-    getLastupdate,
-    renderDate,
-    updateElastedTime,
-    loadElapstedTime,
-    modifyLastUpdate,
-    intToTimer
+  loadProcessingPause,
+  loadAllData,
+  loadProcessingPlay,
+  loadUnsubmitedTask,
+  loadSubmitedTask,
+  setPrevProcessIsOff,
+  setProcessToPause,
+  setProcessToPlay,
+  getLastupdate,
+  renderDate,
+  updateElastedTime,
+  loadElapstedTime,
+  modifyLastUpdate,
+  intToTimer,
 };
