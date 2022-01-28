b150dffb07676629a114d39bb65f2e568be681ec
Mampionona33 <ramamps33@gmail.com>
2022-01-28 11:03:26 +0300

    start to implement handlelClick validate button

--- ui/src/Components/ValidateButton.jsx
+++ ui/src/Components/ValidateButton.jsx
@@ -3,9 +3,19 @@ import { Tooltip, IconButton, Typography, Box } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
 const ValidateButton = () => {
+  const handleClick = (e) => {
+    e.preventDefault;
+    alert('validate button clicked');
+  };
+
   return (
     <Tooltip title='Submit Task' arrow>
-      <IconButton color='primary' component='span' label='Pause button'>
+      <IconButton
+        color='primary'
+        component='span'
+        label='Pause button'
+        onClick={handleClick}
+      >
         <CheckCircleIcon sx={{ fontSize: '30px' }} />
       </IconButton>
     </Tooltip>
