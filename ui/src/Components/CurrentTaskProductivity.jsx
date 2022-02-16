d10faa233555392122cb83d6855ef0fe9245fabd
Mampionona33 <ramamps33@gmail.com>
2022-02-16 09:20:58 +0300

    CurrentTaskProductivity.jsx : finish to get productivity

--- ui/src/Components/CurrentTaskProductivity.jsx
+++ ui/src/Components/CurrentTaskProductivity.jsx
@@ -1,36 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Box, Typography, Divider } from '@mui/material';
-import {
-  loadAllData,
-  loadUnsubmitedTask,
-  fetchTaskType,
-  userLoggedTasks,
-} from './dataHandler';
+import { fetchTaskType, userLoggedTasks } from './dataHandler';
+import { empty } from '@apollo/client';
 
 const CurrentTaskProductivity = () => {
-  const [componentData, setComponentData] = useState([]);
+  const [userTaskListUnsb, setUserTaskListUnsb] = useState([]);
+  //   const [currentTask, setCurrentTask] = useState([]);
+  const [goal, setGoal] = useState(0);
 
-  // fetch all unsubmite task
-  const allUnsubmitedTask = loadUnsubmitedTask();
   // fetch all task type
   const allTaskType = fetchTaskType();
+  // fetch all unsubmite task
   const userDataLoged = userLoggedTasks();
 
-  // get connected user
-  const { loginWithRedirect, logout, user, isLoading } = useAuth0();
-  let userData = [];
-  if (user) {
-    userData = componentData.filter((task) => task.user.email === user.email);
-  }
+  const taskPlay = userTaskListUnsb.filter(
+    (task) => task.processing === 'isPlay'
+  );
+  const taskPause = userTaskListUnsb.filter(
+    (task) => task.processing === 'isPause'
+  );
 
   useEffect(() => {
-    if (allUnsubmitedTask !== undefined) {
-      setComponentData((prev) => allUnsubmitedTask);
+    // Fetch all task unsubmite for the current user
+    if (userDataLoged.length > 0) {
+      setUserTaskListUnsb((prev) => userDataLoged);
     }
-  }, [allUnsubmitedTask]);
 
-  console.log('userDataLoged', userDataLoged);
+    /* if task user list is note empty. make filter to find 
+		pause and play task and asign it to the current task */
+    if (userTaskListUnsb.length > 0 && allTaskType) {
+      if (taskPlay.length > 0 && allTaskType) {
+        const getGoalPlay = allTaskType.filter(
+          (taskType) => taskType.name === taskPlay[0].typeTrav
+        );
+        setGoal((prev) => getGoalPlay[0].objectif);
+      }
+
+      if (taskPause.length > 0) {
+        const getGoalPause = allTaskType.filter(
+          (taskType) => taskType.name === taskPause[0].typeTrav
+        );
+        setGoal((prev) => getGoalPause[0].objectif);
+      }
+    }
+  }, [userDataLoged, userTaskListUnsb]);
+
+  console.log('goal', goal);
   /* console.log('allTaskType', allTaskType);
   console.log('allTaskType', allTaskType); */
 
