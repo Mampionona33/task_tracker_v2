131d18a44cbeb47c76417920cba1d31a479615fd
Mampionona33 <ramamps33@gmail.com>
2022-02-19 14:41:52 +0300

    CurrentTaskSimulator : adding Paper placeholder

--- ui/src/Components/CurrentTaskSimulator.jsx
+++ ui/src/Components/CurrentTaskSimulator.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Box, Typography, TextField } from '@mui/material';
+import { Card, Box, Typography, TextField, Paper } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const CurrentTaskSimulator = (props) => {
@@ -60,45 +60,63 @@ const CurrentTaskSimulator = (props) => {
 
   return (
     <Card elevation={3} sx={{ marginTop: '1rem' }}>
-      <Box display='flex' margin='1rem' gap={2} flexDirection='column'>
-        <Box>
-          <TextField
-            size='small'
-            type='number'
-            label='Nombre Prod Aft'
-            inputProps={{ style: TextFieldStyle, min: '1' }}
-            InputLabelProps={{ shrink: true }}
-            placeholder='Nombre Product After'
-          />
+      <Box
+        display='flex'
+        flexDirection='row'
+        justifyContent='space-evenly'
+        alignItems='center'
+      >
+        <Box display='flex' margin='1rem' gap={2} flexDirection='column'>
+          <Box>
+            <TextField
+              size='small'
+              type='number'
+              label='Nombre Prod Aft'
+              inputProps={{ style: TextFieldStyle, min: '1' }}
+              InputLabelProps={{ shrink: true }}
+              placeholder='Nombre Product After'
+            />
+          </Box>
+          <Box display='flex' gap={1}>
+            <TextField
+              label='hrs'
+              type='number'
+              id='hrs'
+              value={hrs}
+              onChange={handleTimerInputChange}
+              inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '23' }}
+              InputLabelProps={{ shrink: true }}
+            />
+            <TextField
+              label='Min'
+              type='number'
+              id='min'
+              value={min}
+              onChange={handleTimerInputChange}
+              inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '59' }}
+              InputLabelProps={{ shrink: true }}
+            />
+            <TextField
+              label='Sec'
+              id='sec'
+              type='number'
+              value={sec}
+              onChange={handleTimerInputChange}
+              inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '59' }}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Box>
         </Box>
-        <Box display='flex' gap={1}>
-          <TextField
-            label='hrs'
-            type='number'
-            id='hrs'
-            value={hrs}
-            onChange={handleTimerInputChange}
-            inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '23' }}
-            InputLabelProps={{ shrink: true }}
-          />
-          <TextField
-            label='Min'
-            type='number'
-            id='min'
-            value={min}
-            onChange={handleTimerInputChange}
-            inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '59' }}
-            InputLabelProps={{ shrink: true }}
-          />
-          <TextField
-            label='Sec'
-            id='sec'
-            type='number'
-            value={sec}
-            onChange={handleTimerInputChange}
-            inputProps={{ style: TimerTextFieldeStyle, min: '0', max: '59' }}
-            InputLabelProps={{ shrink: true }}
-          />
+        <Box>
+          <Paper
+            sx={{
+              margin: '1rem',
+              padding: '0.5rem',
+              backgroundColor: 'primary.light',
+            }}
+          >
+            <Typography>100%</Typography>
+          </Paper>
         </Box>
       </Box>
     </Card>
