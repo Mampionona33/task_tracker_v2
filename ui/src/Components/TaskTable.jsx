0ae24108807e790ef86e622c74492d7b826964a7
Mampionona33 <ramamps33@gmail.com>
2022-03-18 09:53:40 +0300

    TaskTable.jsx : adding setPrevProcessIsOff on function handleClickPlay button

--- ui/src/Components/TaskTable.jsx
+++ ui/src/Components/TaskTable.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import { Box, Link, IconButton, Card, Typography } from "@mui/material";
-import LinkIcon from "@mui/icons-material/Link";
-import PlayCircleIcon from "@mui/icons-material/PlayCircle";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { LOAD_DATA, FILTRED_FICHE } from "../GraphQL/Queries";
-import { useQuery, gql, refetchQueries, useMutation } from "@apollo/client";
-import { UPDATE_FICHE } from "../GraphQL/Mutation";
-import DialogEditTask from "./DialogEditTask.jsx";
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { Box, Link, IconButton, Card, Typography } from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { LOAD_DATA, FILTRED_FICHE } from '../GraphQL/Queries';
+import { useQuery, gql, refetchQueries, useMutation } from '@apollo/client';
+import { UPDATE_FICHE } from '../GraphQL/Mutation';
+import DialogEditTask from './DialogEditTask.jsx';
 import {
   loadUnsubmitedTask,
   setPrevProcessIsOff,
@@ -16,11 +17,12 @@ import {
   modifyLastUpdate,
   updateElastedTime,
   dateFormater,
-} from "./dataHandler";
+  setProcessToPause,
+} from './dataHandler';
 
 export default function TaskTable() {
   const [sortModel, setSortModel] = useState([
-    { field: "lastUpdate", sort: "desc" },
+    { field: 'lastUpdate', sort: 'desc' },
   ]);
   const [list, setList] = useState([]);
 
@@ -30,153 +32,157 @@ export default function TaskTable() {
   // columns to use inside table
   const columns = [
     {
-      field: "id",
-      headerName: "Id",
-      headerAlign: "center",
+      field: 'id',
+      headerName: 'Id',
+      headerAlign: 'center',
       flex: 1,
       hide: true,
     },
     {
-      field: "statIvpn",
-      headerName: "Status IVPN",
-      headerAlign: "center",
+      field: 'statIvpn',
+      headerName: 'Status IVPN',
+      headerAlign: 'center',
       flex: 1,
       hide: true,
     },
     {
-      field: "comment",
-      headerName: "comment",
-      headerAlign: "center",
+      field: 'comment',
+      headerName: 'comment',
+      headerAlign: 'center',
       flex: 1,
       hide: true,
     },
     {
-      field: "nbBefor",
-      headerName: "Number Before",
-      headerAlign: "center",
+      field: 'nbBefor',
+      headerName: 'Number Before',
+      headerAlign: 'center',
       flex: 1,
       hide: true,
     },
     {
-      field: "nbAft",
-      headerName: "Number After",
-      headerAlign: "center",
+      field: 'nbAft',
+      headerName: 'Number After',
+      headerAlign: 'center',
       flex: 1,
       hide: true,
     },
     {
-      field: "numFiche",
-      headerName: "Num",
-      headerAlign: "center",
+      field: 'numFiche',
+      headerName: 'Num',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 80,
     },
     {
-      field: "typeTrav",
-      headerName: " Task Type",
-      headerAlign: "center",
+      field: 'typeTrav',
+      headerName: ' Task Type',
+      headerAlign: 'center',
       valueFormatter: ({ value }) => `${value}`,
       flex: 1,
       minWidth: 150,
     },
     {
-      field: "statusCom",
-      headerName: "Status Com",
-      headerAlign: "center",
+      field: 'statusCom',
+      headerName: 'Status Com',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 100,
     },
     {
-      field: "state",
-      headerName: "State",
-      headerAlign: "center",
+      field: 'state',
+      headerName: 'State',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 75,
     },
     {
-      field: "lastUpdate",
-      headerName: "Last Update",
-      type: "date",
-      align: "center",
-      headerAlign: "center",
-      hide: "true",
+      field: 'lastUpdate',
+      headerName: 'Last Update',
+      type: 'date',
+      align: 'center',
+      headerAlign: 'center',
+      hide: 'true',
     },
     {
-      field: "productivity",
-      headerName: "Productivity",
-      type: "text",
-      align: "center",
-      headerAlign: "center",
+      field: 'productivity',
+      headerName: 'Productivity',
+      type: 'text',
+      align: 'center',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 90,
     },
     {
-      field: "cat",
-      headerName: "Categorie",
-      type: "text",
-      align: "center",
-      headerAlign: "center",
+      field: 'cat',
+      headerName: 'Categorie',
+      type: 'text',
+      align: 'center',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 80,
     },
     {
-      field: "elapstedTimeRender",
-      headerName: "elapstedTime",
-      type: "text",
-      align: "center",
-      headerAlign: "center",
+      field: 'elapstedTimeRender',
+      headerName: 'elapstedTime',
+      type: 'text',
+      align: 'center',
+      headerAlign: 'center',
       flex: 1,
       minWidth: 150,
     },
     {
-      field: "link",
-      headerName: "Link",
-      type: "link",
-      align: "center",
-      headerAlign: "center",
+      field: 'link',
+      headerName: 'Link',
+      type: 'link',
+      align: 'center',
+      headerAlign: 'center',
       flex: 1,
       renderCell: (params) => (
-        <Link href={params.value} target="_blank">
+        <Link href={params.value} target='_blank'>
           <LinkIcon />
         </Link>
       ),
     },
     {
-      field: "actions",
-      headerName: "Action",
-      align: "center",
-      justifyContent: "space-between",
-      headerAlign: "center",
-      flexWrap: "wrap",
+      field: 'actions',
+      headerName: 'Action',
+      align: 'center',
+      justifyContent: 'space-between',
+      headerAlign: 'center',
+      flexWrap: 'wrap',
       flex: 1,
       minWidth: 200,
       renderCell: (param) => (
         <React.Fragment>
           <IconButton
-            color="primary"
-            component="span"
-            arial-label="Play button"
+            color='primary'
+            component='span'
+            arial-label='Play button'
             // using param and event to get id of the actual fiche
             onClick={(event) => handleClickPlay(event, param)}
             // conditionnal disabling button play on action column
-            disabled={param.row.processing === "isPlay"}
+            // disabled={param.row.processing === 'isPlay'}
           >
-            <PlayCircleIcon />
+            {param.row.processing === 'isPlay' ? (
+              <PauseCircleIcon />
+            ) : (
+              <PlayCircleIcon />
+            )}
           </IconButton>
 
           <IconButton
-            color="primary"
-            component="span"
-            arial-label="Play button"
+            color='primary'
+            component='span'
+            arial-label='Play button'
             onClick={(event) => handleClickEdit(event, param)}
           >
             <EditIcon />
           </IconButton>
 
           <IconButton
-            color="primary"
-            component="span"
-            arial-label="Edit button"
+            color='primary'
+            component='span'
+            arial-label='Edit button'
           >
             <DeleteIcon />
           </IconButton>
@@ -190,11 +196,11 @@ export default function TaskTable() {
     refetchQueries: [LOAD_DATA],
     refetchQueries: [
       FILTRED_FICHE,
-      { variables: { input: { submiteState: "isUnsubmited" } } },
+      { variables: { input: { submiteState: 'isUnsubmited' } } },
     ],
     refetchQueries: [
       FILTRED_FICHE,
-      { variables: { input: { submiteState: "isSubmited" } } },
+      { variables: { input: { submiteState: 'isSubmited' } } },
     ],
     // to execute refetch
     awaitRefetchQueries: true,
@@ -203,26 +209,40 @@ export default function TaskTable() {
   // function to execute on play button click
   const handleClickPlay = async (param, event) => {
     let currentId = event.id;
+    console.log(event.row.processing);
 
     const elapstedTime =
       (Date.parse(new Date()) - Date.parse(arrayRows.lastUpdate)) / 1000 +
       arrayRows.elapstedTime;
 
-    await modifyLastUpdate(prevTaskId[0], fichesUpdate, erroUpDate)
-      .then(setPrevProcessIsOff(prevTaskId[0], fichesUpdate, erroUpDate))
-      .then(
-        updateElastedTime(prevTaskId[0], elapstedTime, fichesUpdate, erroUpDate)
-      )
-      .then(setProcessToPlay(currentId, fichesUpdate, erroUpDate))
-      .then(modifyLastUpdate(currentId, fichesUpdate, erroUpDate))
-      .then(() => {
-        if (event.row.processing === "isPlay") {
-          return;
-        }
-        if (event.row.processing === "isOff") {
-          window.location.href = "#/dashboard";
-        }
-      });
+    if (
+      event.row.processing === 'isPause' ||
+      event.row.processing === 'isOff'
+    ) {
+      await modifyLastUpdate(prevTaskId[0], fichesUpdate, erroUpDate)
+        .then(setPrevProcessIsOff(prevTaskId[0], fichesUpdate, erroUpDate))
+        .then(
+          updateElastedTime(
+            prevTaskId[0],
+            elapstedTime,
+            fichesUpdate,
+            erroUpDate
+          )
+        )
+        .then(setProcessToPlay(currentId, fichesUpdate, erroUpDate))
+        .then(modifyLastUpdate(currentId, fichesUpdate, erroUpDate))
+        .then(() => {
+          if (event.row.processing === 'isPlay') {
+            return;
+          }
+          if (event.row.processing === 'isOff') {
+            window.location.href = '#/dashboard';
+          }
+        });
+    }
+    if (event.row.processing === 'isPlay') {
+      setPrevProcessIsOff(event.id, fichesUpdate, erroUpDate);
+    }
   };
 
   // function to execute on click edit buton
@@ -247,8 +267,8 @@ export default function TaskTable() {
   let prevTask = [];
 
   if (list.length > 0) {
-    taskPlay = list.filter((task) => task.processing === "isPlay");
-    taskPause = list.filter((task) => task.processing === "isPause");
+    taskPlay = list.filter((task) => task.processing === 'isPlay');
+    taskPause = list.filter((task) => task.processing === 'isPause');
   }
 
   if (taskPlay.length > 0) {
@@ -279,11 +299,11 @@ export default function TaskTable() {
     const elapstedTaskPause = item.elapstedTime;
 
     let formatDate;
-    if (item.processing === "isPlay") {
+    if (item.processing === 'isPlay') {
       formatDate = dateFormater(elapstedTaskPlay);
     }
 
-    if (item.processing === "isPause" || item.processing === "isOff") {
+    if (item.processing === 'isPause' || item.processing === 'isOff') {
       formatDate = dateFormater(elapstedTaskPause);
     }
 
@@ -301,7 +321,7 @@ export default function TaskTable() {
       comment: item.comment,
       elapstedTime: item.elapstedTime,
       elapstedTimeRender: `${formatDate.day}:${formatDate.hours}:${formatDate.min}:${formatDate.sec}`,
-      link: item.url != "" ? item.url : "https://www.google.mg/",
+      link: item.url != '' ? item.url : 'https://www.google.mg/',
       processing: item.processing,
     };
 
@@ -312,29 +332,29 @@ export default function TaskTable() {
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "85vh",
-        "& .emptyType": {
-          backgroundColor: "warning.light",
-          color: "warning.contrastText",
+        width: '100%',
+        height: '85vh',
+        '& .emptyType': {
+          backgroundColor: 'warning.light',
+          color: 'warning.contrastText',
         },
-        "& .sby": {
-          backgroundColor: "error.main",
-          color: "error.contrastText",
+        '& .sby': {
+          backgroundColor: 'error.main',
+          color: 'error.contrastText',
         },
       }}
     >
       <Card
         sx={{
-          justifyContent: "center",
-          display: "flex",
-          backgroundColor: "#28B463",
-          color: "#fff",
+          justifyContent: 'center',
+          display: 'flex',
+          backgroundColor: '#28B463',
+          color: '#fff',
         }}
       >
-        <Typography variant="h4">Tasks List</Typography>
+        <Typography variant='h4'>Tasks List</Typography>
       </Card>
-      <Box sx={{ width: "100%", height: "100%" }}>
+      <Box sx={{ width: '100%', height: '100%' }}>
         <DataGrid
           columns={columns}
           pageSize={7}
@@ -342,22 +362,22 @@ export default function TaskTable() {
           rowsPerPageOptions={[7]}
           pagination
           sx={{
-            maxHeight: "80vh",
-            margin: "1rem 5rem",
-            color: "contrastText",
-            backgroundColor: "#fff",
-            boxShadow: "3px 5px 15px 1px rgba(0, 0, 0, 0.3)",
+            maxHeight: '80vh',
+            margin: '1rem 5rem',
+            color: 'contrastText',
+            backgroundColor: '#fff',
+            boxShadow: '3px 5px 15px 1px rgba(0, 0, 0, 0.3)',
           }}
           // Styling cell depanding on it's value
           getCellClassName={(params) => {
-            if (params.value === "Empty Type") {
-              return "emptyType";
+            if (params.value === 'Empty Type') {
+              return 'emptyType';
             }
-            if (params.value === "Sby") {
-              return "sby";
+            if (params.value === 'Sby') {
+              return 'sby';
             }
           }}
-          justifyContent="space-between"
+          justifyContent='space-between'
           // default sorting to show sby on top of list
           sortModel={sortModel}
           onSortModelChange={(model) => setSortModel(model)}
