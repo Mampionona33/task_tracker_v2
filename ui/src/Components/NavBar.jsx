b0960a0a5fb3f91f35ebfe1abc0159b56ce00cda
Mampionona33 <ramamps33@gmail.com>
2022-04-04 14:23:52 +0300

    NavBar.jsx : adding condition in line 95

--- ui/src/Components/NavBar.jsx
+++ ui/src/Components/NavBar.jsx
@@ -92,6 +92,8 @@ export default function Navbar() {
       setWindTitle((prev) => 'Task List');
     } else if (location.pathname.includes('dashboard')) {
       setWindTitle((prev) => 'Dashboard');
+    } else {
+      setWindTitle((prev) => '');
     }
   }, [userData, location]);
 
