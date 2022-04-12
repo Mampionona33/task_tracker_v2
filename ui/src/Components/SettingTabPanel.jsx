0629be43cfe135b67a0d066b86535a5f759f45a2
Mampionona33 <ramamps33@gmail.com>
2022-04-12 14:18:01 +0300

    SettingTabPanle.jsx : adding style in tabs container in line 67

--- ui/src/Components/SettingTabPanel.jsx
+++ ui/src/Components/SettingTabPanel.jsx
@@ -64,11 +64,12 @@ export default function SettingTabPanel(params) {
           borderRight: 1,
           borderColor: 'divider',
           backgroundColor: 'white',
+          '& .MuiTabs-flexContainer': { alignItems: 'flex-start' },
         }}
       >
-        <Tab label='Item One' {...a11yProps(0)} />
-        <Tab label='Item Two' {...a11yProps(1)} />
-        <Tab label='Item Three' {...a11yProps(2)} />
+        <Tab label='Manage Task Type' {...a11yProps(0)} />
+        <Tab label='Manage Commertial Status' {...a11yProps(1)} />
+        <Tab label='Manage IVPN Status' {...a11yProps(2)} />
         <Tab label='Item Four' {...a11yProps(3)} />
         <Tab label='Item Five' {...a11yProps(4)} />
         <Tab label='Item Six' {...a11yProps(5)} />
