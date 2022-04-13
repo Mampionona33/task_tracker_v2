d2a36483663c5ca95e305fca6336d0e67b14814f
Mampionona33 <ramamps33@gmail.com>
2022-04-13 09:27:56 +0300

    settingManageTaskType.jsx : add icon

--- ui/src/Components/SettingManageTaskTypeTable.jsx
+++ ui/src/Components/SettingManageTaskTypeTable.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Box, Link, IconButton, Card, Typography, Paper } from '@mui/material';
+import {
+  Box,
+  Link,
+  IconButton,
+  Card,
+  Typography,
+  Paper,
+  Button,
+} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function SettingManageTaskTypeTable(params) {
   const columns = [
-    { field: 'id', headerName: 'Id', headerAlign: 'center' },
-    { field: 'name', headerName: 'Name', headerAlign: 'center' },
-    { field: 'objectif', headerName: 'Goal', headerAlign: 'center' },
+    { field: 'id', headerName: 'Id', headerAlign: 'center', hide: 'true' },
+    { field: 'name', headerName: 'Name', headerAlign: 'center', flex: 1 },
+    { field: 'objectif', headerName: 'Goal', headerAlign: 'center', flex: 1 },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      headerAlign: 'center',
+      renderCell: (params) => (
+        <React.Fragment>
+          <IconButton color='primary' aria-label='Edit'>
+            <EditIcon />
+          </IconButton>
+          <IconButton color='primary' aria-label='Delete'>
+            <DeleteIcon />
+          </IconButton>
+        </React.Fragment>
+      ),
+    },
   ];
   const rows = [{ id: 0, name: 'test', objectif: 6 }];
   return (
-    <Box height={'85vh'} bgcolor={'#fff'}>
-      <DataGrid columns={columns} rows={rows} />
+    <Box
+      display={'flex'}
+      flexDirection={'row'}
+      justifyContent={'space-between'}
+      width={'75vw'}
+    >
+      <Box height={'75vh'} bgcolor={'#fff'}>
+        <DataGrid columns={columns} rows={rows} />
+      </Box>
+      <Box>
+        <Button variant='contained'>Add new</Button>
+      </Box>
     </Box>
   );
 }
