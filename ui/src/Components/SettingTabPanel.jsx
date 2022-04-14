2e206bb68b0696abcc62840e58fafb0fbbe2cf1f
Mampionona33 <ramamps33@gmail.com>
2022-04-14 12:41:34 +0300

    SettingTabPanel.jsx : adding Box and set it's width to 5vw in line 119

--- ui/src/Components/SettingTabPanel.jsx
+++ ui/src/Components/SettingTabPanel.jsx
@@ -89,6 +89,7 @@ export default function SettingTabPanel(params) {
         flexGrow: 1,
         bgcolor: 'background.paper',
         display: 'flex',
+        justifyContent: 'space-between',
         height: 'calc(100vh - 48px)',
         background:
           'linear-gradient(55deg, rgba(112,128,144,1) 0%, rgba(192,192,192,1) 36%, rgba(192,192,192,1) 62%, rgba(112,128,144,1) 100%)',
@@ -115,31 +116,33 @@ export default function SettingTabPanel(params) {
         <Tab label='Item Six' {...a11yProps(5)} />
         <Tab label='Item Seven' {...a11yProps(6)} />
       </Tabs>
-      <TabPanel value={value} index={0}>
-        <SettingManageData
-          columns={taskTypeColumns}
-          rows={tastTypeRows}
-          dataType={'Task'}
-        />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+      <Box display={'flex'} marginRight={'5vw'}>
+        <TabPanel value={value} index={0}>
+          <SettingManageData
+            columns={taskTypeColumns}
+            rows={tastTypeRows}
+            dataType={'Task'}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          Item Seven
+        </TabPanel>
+      </Box>
     </Box>
   );
 }
