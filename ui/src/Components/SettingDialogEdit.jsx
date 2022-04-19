0fc384fbbc696be8f7ee772a379b0e0187ede088
Mampionona33 <ramamps33@gmail.com>
2022-04-19 14:49:59 +0300

    SettingDialogEdit.jsx : adding Diveder on line 44

--- ui/src/Components/SettingDialogEdit.jsx
+++ ui/src/Components/SettingDialogEdit.jsx
@@ -31,9 +31,17 @@ export default function SettingDialogEdit({
     const refGoal = useRef(null);
     const refTaskTypeName = useRef(null);
 
+    useEffect(() => {
+      if (selectedRowdata !== undefined) {
+        setGoal((prev) => selectedRowdata.objectif);
+        setTaskTypeName((prev) => selectedRowdata.name);
+      }
+    }, [selectedRowdata]);
+
     return (
       <React.Fragment>
         <DialogTitle>{dialogTitle}</DialogTitle>
+        <Divider />
         <DialogContent>
           <Box display={'flex'} flexDirection='column' gap={'1rem'}>
             <Box display={'flex'} gap={'1rem'}>
@@ -41,13 +49,16 @@ export default function SettingDialogEdit({
                 label='Task type name'
                 type={'text'}
                 placeholder='Write here the task type name'
+                value={taskTypeName}
               />
               <TextField
                 label='Goal'
                 type={'number'}
                 placeholder='Goal per hours'
+                value={goal}
               />
             </Box>
+
             <Box
               display={'flex'}
               flexDirection={'row'}
