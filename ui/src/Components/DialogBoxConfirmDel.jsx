67f527f2db15c987d83a232f71a9d32cd396582e
Mampionona33 <ramamps33@gmail.com>
2022-05-05 10:33:27 +0300

    DialogBoxConfirmDel.jsx : adding customTypography in line 26

--- ui/src/Components/DialogBoxConfirmDel.jsx
+++ ui/src/Components/DialogBoxConfirmDel.jsx
@@ -5,8 +5,9 @@ import {
   DialogContent,
   DialogTitle,
   Divider,
+  Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { deletTaskType } from './dataHandler';
 
 export default function DialogBoxConfirmDel({
@@ -18,24 +19,36 @@ export default function DialogBoxConfirmDel({
 }) {
   const handleConfirm = async () => {
     if (title.includes('Task Type')) {
-      console.log('task type data', data);
+      console.log('task type data', data.name);
     }
   };
+
+  const CustomTypography = () => {
+    if (title.includes('Task Type')) {
+      return (
+        <Typography>
+          Do you realy want to delete {data.name} : {data.objectif} from database ?
+        </Typography>
+      );
+    }
+  };
+
   return (
     <React.Fragment>
       <Dialog open={open} onClose={close}>
         <DialogTitle>{title}</DialogTitle>
         <Divider />
         <DialogContent>
-          <DialogActions>
-            <Button variant='outlined' onClick={handleConfirm}>
-              Confirm
-            </Button>
-            <Button variant='outlined' onClick={close}>
-              Cancel
-            </Button>
-          </DialogActions>
+          <CustomTypography />
         </DialogContent>
+        <DialogActions>
+          <Button variant='outlined' onClick={handleConfirm}>
+            Confirm
+          </Button>
+          <Button variant='outlined' onClick={close}>
+            Cancel
+          </Button>
+        </DialogActions>
       </Dialog>
     </React.Fragment>
   );
